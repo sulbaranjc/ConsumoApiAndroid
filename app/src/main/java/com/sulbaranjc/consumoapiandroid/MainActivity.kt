f7b3887489b4722178b29f1510c5792f507555bd
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2026-01-25 17:40:26 +0100

    probar el cambio de binding ok

--- app/src/main/java/com/sulbaranjc/consumoapiandroid/MainActivity.kt
+++ app/src/main/java/com/sulbaranjc/consumoapiandroid/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.sulbaranjc.consumoapiandroid
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.sulbaranjc.consumoapiandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Aquí luego irá:
+        // - RecyclerView
+        // - llamada a Retrofit
     }
 }
